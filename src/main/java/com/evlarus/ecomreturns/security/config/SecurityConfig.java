2d07d9943d7705449f2694f42a906de5e4063344
evlarus <yauheni.bayarchyk@gmail.com>
2026-08-26 01:26:24 +0300

    drop unused method security annotation for now

--- src/main/java/com/evlarus/ecomreturns/security/config/SecurityConfig.java
+++ src/main/java/com/evlarus/ecomreturns/security/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.evlarus.ecomreturns.security.config;
 import com.evlarus.ecomreturns.security.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,7 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
 
